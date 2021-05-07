f03961c5d1d62c9b1d9d94408ed169d227ec24c8
Diggerati <innocentmasuki99@gmail.com>
2021-05-07 15:12:19 +0300

    nav changes

--- js/main.js
+++ js/main.js
@@ -1,12 +1,30 @@
+var tabs = document.getElementsByClassName("nav-link");
+
+
 document.addEventListener("DOMContentLoaded", function() {
     window.addEventListener('scroll', function() {
-        if (window.scrollY > 50) {
-            document.getElementById('top_nav').style.display = "block";
 
-        } else {
-            document.getElementById('top_nav').style.display = "none";
+        for (var i = 0; i < x.length; i++) {
+            if (tabs[i].classList.contains("myaccount")) {
+                if (window.scrollY > 50) {
+                    document.getElementById('top_nav').style.display = "block";
 
+                } else {
+                    document.getElementById('top_nav').style.display = "none";
+
+                }
+            } else {
+                document.getElementById('top_nav').style.display = "block";
+            }
         }
+
+
+
+
+
     });
 });
+
+
+
 // DOMContentLoaded  end
