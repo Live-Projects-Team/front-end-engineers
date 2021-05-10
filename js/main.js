4c804879cc9bea33d79d61282662b224a1b9e8ba
Innocent Masuki <innocentmasuki99@gmail.com>
2021-05-10 21:22:35 +0300

    reset

--- js/main.js
+++ js/main.js
@@ -1,30 +1,15 @@
-var tabs = document.getElementsByClassName("nav-link");
-
-
 document.addEventListener("DOMContentLoaded", function() {
     window.addEventListener('scroll', function() {
-
-        for (var i = 0; i < x.length; i++) {
-            if (tabs[i].classList.contains("myaccount")) {
-                if (window.scrollY > 50) {
-                    document.getElementById('top_nav').style.display = "block";
-
-                } else {
-                    document.getElementById('top_nav').style.display = "none";
-
-                }
-            } else {
-                document.getElementById('top_nav').style.display = "block";
-            }
+        if (window.scrollY > 50) {
+            document.getElementById('navbar_top').classList.add('fixed-top');
+            // add padding top to show content behind navbar
+            navbar_height = document.querySelector('.navbar').offsetHeight;
+            document.body.style.paddingTop = navbar_height + 'px';
+        } else {
+            document.getElementById('navbar_top').classList.remove('fixed-top');
+            // remove padding top from body
+            document.body.style.paddingTop = '0';
         }
-
-
-
-
-
     });
 });
-
-
-
 // DOMContentLoaded  end
