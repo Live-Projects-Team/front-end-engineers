c6c7f833ab6d927f6a9d3f4d46f993179dc86534
pyskywalker <79524205+pyskywalker@users.noreply.github.com>
2021-05-15 10:16:21 +0300

    added .php and dates

---
+++ contact_us.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Contact us</title>
+    <link rel="stylesheet" type="text/css" href="./css/contact_us.css">
+    <link href="https://fonts.googleapis.com/css?family=Quicksand&display=swap" rel="stylesheet">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css">
+</head>
+
+<body>
+    <div class="container">
+        <div class="contact-box">
+
+            <div class="left contact-section">
+                <div class="contact-info">
+                    <div><i class="fas fa-map-marker-alt"></i>UDOM, Dodoma, Tanzania</div>
+                    <div><i class="fas fa-envelope"></i>contact@email.com</div>
+                    <div><i class="fas fa-phone"></i>+255-7000 000 00</div>
+                    <div><i class="fas fa-clock"></i>Mon - Fri 8:00 AM to 5:00 PM</div>
+                </div>
+            </div>
+
+
+            <div class="right">
+                <h2>Contact Us</h2>
+                <input type="text" class="field" placeholder="Your Name">
+                <input type="text" class="field" placeholder="Your Email">
+                <input type="text" class="field" placeholder="Phone">
+                <textarea placeholder="Message" class="field"></textarea>
+                <button class="btn">Send</button>
+            </div>
+        </div>
+</body>
+
+</html>
